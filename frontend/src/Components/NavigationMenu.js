e80424705ab16551eb9783cad6a8499acabde590
Reimo Saar <reimosaar135@gmail.com>
2020-08-09 13:30:21 +0300

    Navigation menu improvement

--- frontend/src/Components/NavigationMenu.js
+++ frontend/src/Components/NavigationMenu.js
@@ -1,28 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "../Style/Components/NavigationMenu.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faStore } from '@fortawesome/free-solid-svg-icons'
 
-function NavigationMenu(props) {
+function NavigationMenu({ closeMenu }) {
     return (
         <div className="navigation-menu">
+            <FontAwesomeIcon class="navigation-menu__nav-button" icon={faBars} onClick={closeMenu} />
             <h2 className="navigation-menu__title">Menu</h2>
-            <p>
+            <div>
                 <Link
                     to="/"
-                    onClick={props.closeMenu}
-                    className="navigation-menu__text">
+                    onClick={closeMenu}
+                    className="navigation-menu__link">
+                    <FontAwesomeIcon icon={faStore} className="navigation-menu__icon" />
                     Products
                 </Link>
-            </p>
+            </div>
             <hr></hr>
-            <p>
+            <div>
                 <Link
                     to="/cart"
-                    onClick={props.closeMenu}
-                    className="navigation-menu__text">
+                    onClick={closeMenu}
+                    className="navigation-menu__link">
+                    <FontAwesomeIcon icon={faShoppingCart} className="navigation-menu__icon" />
                     Cart
                 </Link>
-            </p>
+            </div>
             <hr></hr>
         </div>
     )
